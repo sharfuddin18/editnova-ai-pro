50630bb2af7b72b833507137c8e4a8e47e694528
Sharfuddin Ahmed <sharfuddinahmed315@gmail.com>
2025-08-04 16:43:44 +0000

    Add platform-specific files for Flutter desktop and mobile support
    - Generated Android, iOS, Linux, macOS, and Windows folders and configs via `flutter create .`
    - Added platform resources, native entrypoints, manifest files, and icons for all mobile and desktop targets
    - Updated metadata for cross-platform project structure

---
+++ android/app/src/main/kotlin/com/example/editnova/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.editnova
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,24 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_picker
+import file_selector_macos
+import flutter_tts
+import path_provider_foundation
+import shared_preferences_foundation
+import speech_to_text
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
